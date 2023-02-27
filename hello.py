b98a6ee4d1e4f394f8f8383045fba77fe49f4453
ParralelWorlds <jeanvanzyl4@gmail.com>
2023-02-27 16:48:27 +0300

    Initial commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+import os
+os.system('clear')
+
+num = 11
+
+if (num > 10):
+	print('number greater then 10')
+elif(num == 10):
+	print('number equal then 10')
+else:
+	print('number smaller then 10')
